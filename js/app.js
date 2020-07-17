06c3f1c88bfcb1e24f12b93cbe68da1d64083248
Md. Alamgir Hossain <alamgirakash2000@gmail.com>
2020-07-17 23:49:37 +0600

    p

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var word=['Cat dog', 'Dhaka Bangladesh','Polao Biriyani','Warm Hole','Game Of Thrones','Breaking Bad','End Game']
-var guessLimit=[3,4,5,3,4,4,3,3]
-var randNumber=Math.floor(Math.random()*8)
+var guessLimit=[3,4,5,3,4,4,3]
+var randNumber=Math.floor(Math.random()*7)
 
 var game= new Hangman(word[randNumber], guessLimit[randNumber])
 
